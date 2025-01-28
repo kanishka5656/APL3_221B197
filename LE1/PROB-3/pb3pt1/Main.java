d4569a0d89449dbafd2c59ad6197c1c536ca1f6d
221B197 <221B197@juet.co.in>
2025-01-28 11:57:55 +0530

    Add LE1

---
+++ LE1/PROB-3/pb3pt1/Main.java
@@ -0,0 +1,10 @@
+
+class Main {
+    public static void main(String[] args) {
+        Mother m1 = new Child();
+        m1.show();  
+
+        Mother m2 = new Mother();
+        m2.show();  
+    }
+}
---
+++ LE1/PROB-4/pb4/One.java
@@ -0,0 +1,8 @@
+class One {
+    int x;
+    public One(int x) {
+        this.x = x;
+        System.out.println("One's constructor called with value: " + x);
+    }
+}
+

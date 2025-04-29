89d0afc104fe05a57103ed896edee8a62e77f3d3
221B197 <221B197@juet.co.in>
2025-04-29 11:28:04 +0530

    add

---
+++ LE8/Crow.java
@@ -0,0 +1,17 @@
+public class Crow
+{
+    public void cry()
+    {
+        System.out.println("Cawing");
+    }
+    
+    public void eat()
+    {
+        System.out.println("Eating something");
+    }
+    
+    public void fly()
+    {
+        System.out.println("Flying over the mountains");
+    }
+}
